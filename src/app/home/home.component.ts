88afaf79a3ad040fabbb23115e6be5fac4269c20
Levin E P <levinrockx@gmail.com>
2019-05-01 18:30:05 +0530

    Linting changes
    Tslint

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,32 +11,32 @@ export class HomeComponent implements OnInit {
     {
       date: "17th March 2019",
       title: "Heading of Blog Post One",
-      desc:"Lorem ipsum is a mockup text used for webdesign and other things..",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
     },
     {
       date: "18th March 2019",
       title: "Heading of Blog Post Two",
-      desc:"Lorem ipsum is a mockup text used for webdesign and other things..",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
     },
     {
       date: "19th March 2019",
       title: "Heading of Blog Post Three",
-      desc:"Lorem ipsum is a mockup text used for webdesign and other things..",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
     },
     {
       date: "20th March 2019",
       title: "Heading of Blog Post Four",
-      desc:"Lorem ipsum is a mockup text used for webdesign and other things..",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
     },
     {
       date: "21th March 2019",
       title: "Heading of Blog Post Five",
-      desc:"Lorem ipsum is a mockup text used for webdesign and other things..",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
     },
     {
       date: "22nd March 2019",
       title: "Heading of Blog Post Six",
-      desc:"Lorem ipsum is a mockup text used for webdesign and other things..",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
     },
   ];
   constructor() { }
