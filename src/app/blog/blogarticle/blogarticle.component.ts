17ff4633b99ce6a8396e93572275b10742402196
dev-ashwin-kumar <ashwinkarivellur74@gmail.com>
2019-05-27 21:07:35 +0530

    related-post
    still need work

--- src/app/blog/blogarticle/blogarticle.component.ts
+++ src/app/blog/blogarticle/blogarticle.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-blogarticle',
   templateUrl: './blogarticle.component.html',
   styleUrls: ['./blogarticle.component.scss']
 })
 export class BlogarticleComponent implements OnInit {
-
-  constructor() { }
+  public cardList;
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
+    this.getBlogAll();
   }
+  getBlogAll() {
+    this.http.get('http://127.0.0.1:5000/blogall').subscribe((response) => {
+      this.cardList = response;
+    });
+  }
+
 
 }
