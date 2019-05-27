2b45e359bdd633bac0c67b06e8b167fb26edce84
Alwin <alwindevasialh13@gmail.com>
2019-05-27 16:39:40 +0530

    blog-grid

--- src/app/blog/blog-all/blog-all.component.ts
+++ src/app/blog/blog-all/blog-all.component.ts
@@ -6,15 +6,44 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./blog-all.component.scss']
 })
 export class BlogAllComponent implements OnInit {
-  public cardList;
+  public cardList = [
+    {
+      date: "17th March 2019",
+      title: "Heading of Blog Post One",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
+    },
+    {
+      date: "18th March 2019",
+      title: "Heading of Blog Post Two",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
+    },
+    {
+      date: "19th March 2019",
+      title: "Heading of Blog Post Three",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
+    },
+    {
+      date: "20th March 2019",
+      title: "Heading of Blog Post Four",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
+    },
+    {
+      date: "21th March 2019",
+      title: "Heading of Blog Post Five",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
+    },
+    {
+      date: "22nd March 2019",
+      title: "Heading of Blog Post Six",
+      desc: "Lorem ipsum is a mockup text used for webdesign and other things..",
+    },
+  ];
   constructor( private http: HttpClient,) { }
 
   ngOnInit() {
-    this.getBlogAll();
+    
   }
-  getBlogAll() {
-    this.http.get('http://127.0.0.1:5000/blogall').subscribe((response) => {
-      this.cardList = response;
-    });
-  }
+  
+  
+  
 }
