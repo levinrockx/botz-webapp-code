4cc4b343b26b6383d33f0d55fa851e09de7a2167
Levin E P <levinrockx@gmail.com>
2019-06-11 18:47:21 +0530

    About page animation
    About page animation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { Power3 } from 'gsap/all';
+import { Power3, Back, Expo } from 'gsap/all';
 declare var TweenMax: any;
 @Component({
   selector: 'app-about',
@@ -7,17 +7,25 @@ declare var TweenMax: any;
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  @ViewChild('rowOne', { static: true }) rowOne: ElementRef;
+  @ViewChild('rowTwo', { static: true }) rowTwo: ElementRef;
+  @ViewChild('containerTwo', { static: true }) containerTwo: ElementRef;
   @ViewChild('heading', { static: true }) heading: ElementRef;
   @ViewChild('paragraph', { static: true }) paragraph: ElementRef;
   constructor() { }
 
   ngOnInit() {
-    this.animateHeading();
+    this.startAnimation();
   }
 
-  animateHeading() {
-    TweenMax.fromTo(this.heading.nativeElement, 1, {y: 50}, {y: 0, ease: Power3.easeOut});
-    TweenMax.fromTo(this.paragraph.nativeElement, 1, {y: 100}, {y: 0, ease: Power3.easeOut});
+  startAnimation() {
+    TweenMax.fromTo(this.rowTwo.nativeElement, 1, {opacity: 1, top: -2000}, {top: 0, ease: Expo.easeInOut});
+    setTimeout(() => {
+      TweenMax.fromTo(this.rowOne.nativeElement, 1, {opacity: 0}, {opacity: 1, ease: Power3.easeOut});
+      TweenMax.fromTo(this.heading.nativeElement, 1, {y: 50}, {y: 0, ease: Power3.easeOut});
+      TweenMax.fromTo(this.paragraph.nativeElement, 1, {y: 100}, {y: 0, ease: Power3.easeOut});
+      TweenMax.fromTo(this.containerTwo.nativeElement, 1, {opacity: 1}, {top: 0, ease: Power3.easeOut});
+    }, 600);
   }
 
 }
