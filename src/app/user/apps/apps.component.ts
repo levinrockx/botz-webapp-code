7c96cfaf0b56f46bd443f784cd35e017bd73b6e7
Levin E P <levinrockx@gmail.com>
2019-07-18 00:35:41 +0530

    Resistance Finder App
    Resistance Finder App

--- src/app/user/apps/apps.component.ts
+++ src/app/user/apps/apps.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Power4, Back, Expo } from 'gsap/all';
+import { Router } from '@angular/router';
 import { viewClassName } from '@angular/compiler';
 declare var TweenMax: any;
 
@@ -13,15 +14,23 @@ export class AppsComponent implements OnInit {
   @ViewChild('card', { static: true }) card: ElementRef;
   @ViewChild('wrapper', { static: true }) wrapper: ElementRef;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
     this.startAnimation();
   }
+
   startAnimation() {
     TweenMax.fromTo(this.heading.nativeElement, 1, { opacity: 0, y: 50 }, { opacity: 1, y: 0, ease: Power4.easeOut });
     TweenMax.fromTo(this.card.nativeElement, 1, { opacity: 0, y: 100 }, { opacity: 1, y: 0, ease: Power4.easeOut, delay: 0.5 });
     TweenMax.fromTo(this.wrapper.nativeElement, 1, { opacity: 0, y: 100 }, { opacity: 1, y: 0, ease: Power4.easeOut, delay: 0.5 });
   }
+
+  reRoute(routeName) {
+    this.router.navigate([routeName]);
+  }
+
 }
 
