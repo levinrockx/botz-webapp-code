a7fcff4ba85b6eb7f6ccf6011d0f75939a7d1410
Levin E P <levinrockx@gmail.com>
2019-07-18 00:41:35 +0530

    Version Number v0.1.9 change
    Version Number v0.1.9 change

--- src/app/Config/config.service.ts
+++ src/app/Config/config.service.ts
@@ -19,7 +19,7 @@ export class ConfigService {
     contactus: '/contactus',
   };
 
-  public VERSION_NUMBER = 'v0.1.6';
+  public VERSION_NUMBER = 'v0.1.9';
 
   public TOCKEN = {
     keyname : 'tocken',
